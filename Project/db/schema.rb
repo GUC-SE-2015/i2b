065ac1b536a7f48b32d4d57aa7f855281257bc6b
Hussien Kamal <hussienelhady14@gmail.com>
2015-04-04 17:48:31 +0200

    Creating the table of the pitch library and adding two tabs (one for the admins to inserts pitches in the database and one for all users to view the pitch library)

--- Project/db/schema.rb
+++ Project/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404095246) do
+ActiveRecord::Schema.define(version: 20150404144010) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20150404095246) do
     t.datetime "end_time"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "pitches", force: :cascade do |t|
+    t.string   "title"
+    t.string   "brief_description"
+    t.string   "full_description"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "users", force: :cascade do |t|
