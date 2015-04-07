20e67bb5564ed88c61eb5359632fda8216f822cc
Bergitte S. Anwar <bergittesaeed@gmail.com>
2015-04-08 01:49:11 +0200

    Modified Comments

--- Project/config/routes.rb
+++ Project/config/routes.rb
@@ -69,4 +69,12 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+ActionController::Routing::Routes.draw do |map|
+  map.resources :posts, :has_many => :comments
+
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
+  map.root :controller => "post"
+end
+
 end
