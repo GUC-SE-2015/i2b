730e6f4dceaec15406f7f251cb65a5f977647c10
JirayrB <jirayr.benohanian@gmail.com>
2015-04-08 14:56:42 +0200

    Consultancies in the pitch and minor modifications

--- Project/app/controllers/pitches_controller.rb
+++ Project/app/controllers/pitches_controller.rb
@@ -1,29 +1,34 @@
 class PitchesController < ApplicationController
+
   respond_to :html, :json
 
-    http_basic_authenticate_with name: "nageeb", password: "12345678", except: [:index, :show]
+    # http_basic_authenticate_with name: "nageeb", password: "12345678", except: [:index, :show]
 
   def index
       @pitches = Pitch.all
   end
 
   def show
-    @pitch = Pitch.find(params[:id])
+    if  current_user.is_Ideator || current_user.is_Idea_Consultant || current_user.admin
+        @pitch = Pitch.find(params[:id])
+        @consultancies = @pitch.consultancies
+    else
+      redirect_to :back
+    end
   end
 
   def new
-      @pitch = Pitch.new
+    @pitch = Pitch.new
   end
 
 
   def create
-      @pitch = Pitch.new(pitch_params)
- 
-      if @pitch.save
-        redirect_to @pitch
-      else
-        render 'new'
-      end
+    @pitch = Pitch.new(pitch_params)
+    if @pitch.save
+      redirect_to @pitch
+    else
+      render 'new'
+    end
   end
 
 
@@ -31,11 +36,8 @@ class PitchesController < ApplicationController
       @pitch = Pitch.find(params[:id])
   end
 
-  
-
   def update
       @pitch = Pitch.find(params[:id])
- 
       if @pitch.update(pitch_params)
         redirect_to @pitch
       else
@@ -52,9 +54,8 @@ class PitchesController < ApplicationController
   end
 
 
-  private
+  #private
   def pitch_params
-    params.require(:pitch).permit(:title, :brief_description, :full_description, :url, :email)
+    params.require(:pitch).permit(:title, :brief_description, :full_description, :url, :email, :username)
   end
 end
-
--- Project/app/models/pitch.rb
+++ Project/app/models/pitch.rb
@@ -1,2 +1,9 @@
 class Pitch < ActiveRecord::Base
+	def user
+		return User.find_by_email(self.email)
+	end
+
+	def consultancies
+		return Consultancy.where("pitch_id =?", self.id)
+	end
 end
