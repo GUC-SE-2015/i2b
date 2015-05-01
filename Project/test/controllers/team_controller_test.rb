ae8340d7d9530314dd1dc9b92799a174abb782f8
Monicagamal <monicagyoussef@gmail.com>
2015-05-01 03:58:54 +0300

    Test team controller

--- Project/test/controllers/team_controller_test.rb
+++ Project/test/controllers/team_controller_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
 class TeamControllerTest < ActionController::TestCase
- #RSpec.describe TeamsController do
-  #describe "GET index" do
-   # it "assigns @teams" do
-    #  team = Team.create
-     # get :index
-      #expect(assigns(:teams)).to eq([team])
-    #end
+ RSpec.describe TeamsController do
+  describe "GET index" do
+    it "assigns @teams" do
+     team = Team.create
+     get :index
+      expect(assigns(:teams)).to eq([team])
+    end
 
-    #it "renders the index template" do
-     # get :index
-      #expect(response).to render_template("index")
-    #end
-  #end
-#end
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+end
 end
